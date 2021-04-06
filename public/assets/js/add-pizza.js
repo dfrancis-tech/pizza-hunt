e44e9e526c46da5ebb5a70cb3e8686b45ecc17de
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-04-06 16:47:35 -0400

    use indexeddb for offline PWA

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -68,8 +68,54 @@ const handlePizzaSubmit = event => {
     })
     .catch(err => {
       console.log(err);
+      // save in indexedDB
+      saveRecord(formData);
     });
 };
+
+// upload the indexedDB data to MondoDB when online
+function uploadPizza() {
+  // open a transaction on your db
+  const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+  // access your object store
+  const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+  // get all records from store and set to a variable
+  const getAll = pizzaObjectStore.getAll();
+
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/pizzas', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_pizza'], 'readwrite');
+          // access the new_pizza object store
+          const pizzaObjectStore = transaction.objectStore('new_pizza');
+          // clear all items in your store
+          pizzaObjectStore.clear();
+
+          alert('All saved pizza has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+}
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
 $addToppingBtn.addEventListener('click', handleAddTopping);
