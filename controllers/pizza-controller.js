b481a40b6f1b8eae1a2f04b02b15123592839dae
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-04-05 23:48:04 -0400

    return new pizza first

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -10,6 +10,7 @@ const pizzaController = {
             select: '-__v'
           })
           .select('-__v')
+          .sort({ _id: -1 })
           .then(dbPizzaData => res.json(dbPizzaData))
           .catch(err => {
             console.log(err);
